5b9c8368c75871f3c7f7ddfb3e914c3942a2b6ea
David Bickel <david.bickel@vub.be>
2024-04-22 11:27:33 +0200

    Minor fixes, added checks for file format

--- src/bio2byte/mdutils/filetypes/gmx/xtc.py
+++ src/bio2byte/mdutils/filetypes/gmx/xtc.py
@@ -20,43 +20,40 @@ class XTCParsingError(ValueError):
 
 class GromacsXTC:
 
-    # Many thanks to Tsjerk:
-    # https://github.com/Tsjerk/xtc/blob/master/xtc.py
     XTC_FRAME_FORMAT = (
         ">"
-        "l"             # Magic number (4) == 1995
-        "l"             # Atoms (4)
-        "l"             # Step (4)
-        "f"             # Time (4)
-        "fffffffff"     # Box (9*4)
-        "l"             # Atoms (4), checked to be equal to above
-        "f"             # PRecision (4)
-        "lllllll"       # Extent (6*4), MIN: x,y,z, MAX: x,y,z
-        "l"             # Size in bytes (4)
+        "l"             # a) Magic number (4) == 1995
+        "l"             # b) Atoms (4)
+        "l"             # c) Step (4)
+        "f"             # d) Time (4)
+        "fffffffff"     # e-m) Box (9*4)
+        "l"             # n) Atoms (4), checked to be equal to b)
+        "f"             # o) Precision (4)
+        "llllll"        # p-u) Extent (6*4), MIN: x,y,z, MAX: x,y,z
+        "l"             # v) Size in bytes (4)
         # ... Compressed coordinates
     )
 
     def __init__(self, filename: str):
         self.filename = filename
         self._fhandle = open(filename, "rb")
+        self._check()
         self._pointers = list(self._iter_frame_pointers())
         self.goto(0)
         (
-            magicnumber,    # Magic number
+            _,              # Magic number
             self.natoms,    # Number of atoms in the system
             self.step0,     # First step in the XTC file
             self.time0      # First time in the XTC file [ps]
         ) = struct.unpack(self.XTC_FRAME_FORMAT[:5], self.read(16))
         self.goto(-1)
         (
-            magicnumber,    # Magic number
+            _,              # Magic number
             self.natoms,    # Number of atoms in the system
             self.step1,     # Last step in the XTC file
             self.time1      # Last time in the XTC file [ps]
         ) = struct.unpack(self.XTC_FRAME_FORMAT[:5], self.read(16))
         self.goto(0)        # Reset pointer
-        if magicnumber != 1995:
-            raise XTCParsingError("Unexpected magic number in XTC file: {0}".format(magicnumber))
 
     @property
     def nframes(self):
@@ -71,50 +68,63 @@ class GromacsXTC:
         self._fhandle.seek(self._pointers[index])
         return self._fhandle.tell()
     
-    def seek(self, pos: int):
+    def seek(self, offset: int, whence: int=0):
         """Goto position in XTC file"""
-        self._fhandle.seek(pos)
+        self._fhandle.seek(offset, whence)
         return self._fhandle.tell()
 
     def read(self, n_bytes=None):
         """Read n_bytes starting from the current file location"""
         return self._fhandle.read(n_bytes)
+    
+    def tell(self):
+        """Returns the current position in the XTC file"""
+        return self._fhandle.tell()
 
     def _iter_frame_pointers(self, bytebuffer=10007):
+        """Iterates over the file and locates the positions of all frame headers"""
         pos = 0
         self.seek(pos)
-        tag = self.read(8)    # Fixed tag (MagicNumber + NumberOfAtoms)
-
+        tag = self.read(8)  # Fixed tag: Magic number + Number of atoms
+        
         self.seek(pos)
         buffer = self.read(bytebuffer)        
         while len(buffer) >= 56:
-            idx = buffer.find(tag)      # Find tag in buffer
-            try:
-                # Assert that there is a match
-                assert idx > -1
-
-                # Verify match by checking if NumberOfAtoms is repeated at the right position
-                if len(buffer) < idx+56:
-                    buffer += self.read(56)
-                assert buffer[idx+52:idx+56] == tag[4:]
-                
-                # Match found: yield position and move on
+            # Search tag in buffer
+            idx = buffer.find(tag)
+            # If tag was found at end of buffer, extend buffer to include n)
+            if len(buffer) < idx+56:
+                buffer += self.read(56)
+            # If tag was found and b) == n), yield the position and move on
+            if idx > -1 and buffer[idx+52:idx+56] == tag[4:]:
                 pos += idx
                 yield pos
-                buffer = buffer[idx+56:]
-                pos += 56
-
-            except AssertionError:
-                # No match found: move on
-                if len(buffer) >= 56:
-                    pos += len(buffer) - 55
-                    buffer = buffer[-55:]
-
+                buffer, pos = buffer[idx+56:], pos + 56
+            # If tag was not found, move on
+            elif len(buffer) >= 56:
+                buffer, pos = buffer[-55:], pos + len(buffer) - 55
+            # Extend reading buffer if needed
             if len(buffer) < 56:
                 buffer += self.read(bytebuffer)
 
+    def _check(self):
+        """Checks if file adheres to XTC format"""
+        last_position = self.tell()
+        buffer = self.read(struct.calcsize(self.XTC_FRAME_FORMAT))
+        header = struct.unpack(self.XTC_FRAME_FORMAT, buffer)
+        # Check magic number
+        if header[0] != 1995:
+            raise XTCParsingError("Unexpected magic number encountered: {0:d}".format(
+                header[0]))
+        # Check number of atoms, match between b) and n)
+        elif header[1] != header[13]:
+            raise XTCParsingError("Atom numbers do not match: {0:d} != {1:d}".format(
+                header[1], header[13]))
+        self.seek(last_position)
+
+
 def main(args):
-    sys.stderr.write("#File\tn_atoms\tn_frames\n")
+    sys.stderr.write("#File\tNumOfAtoms\tNumOfFrames\n")
     for fname in args:
         xtc = GromacsXTC(fname)
         sys.stdout.write("{0}\t{1:d}\t{2:d}\n".format(fname, xtc.natoms, xtc.nframes))
